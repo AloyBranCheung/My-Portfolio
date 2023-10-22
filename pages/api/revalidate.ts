93bb209234e3c72298bd8e888c4d9bd10bbe5025
bcheung <brandon.cheung.dev@gmail.com>
2023-10-22 16:15:47 -0400

    cors

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -4,6 +4,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+
   // Check for secret to confirm this is a valid request
   if (req?.query?.secret !== process.env.REVALIDATE_TOKEN) {
     return res.status(401).json({ message: "Invalid token" });
