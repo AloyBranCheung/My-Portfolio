f5d3d37c80ede18e65f1e3c296202bd0678c9cfb
bcheung <brandon.cheung.dev@gmail.com>
2023-10-22 16:31:16 -0400

    remove lifecycle

--- cms/src/util/revalidateNext.js
+++ cms/src/util/revalidateNext.js
@@ -1,15 +1,16 @@
-const axios = require("axios");
+// const axios = require("axios");
 
 module.exports = async function () {
   try {
-    await axios.get(
-      `${strapi.config.get("envs.nextjs.nextBaseUrl")}/api/revalidate`,
-      {
-        params: {
-          secret: strapi.config.get("envs.nextjs.revalidateToken"),
-        },
-      }
-    );
+    // await axios.get(
+    //   `${strapi.config.get("envs.nextjs.nextBaseUrl")}/api/revalidate`,
+    //   {
+    //     params: {
+    //       secret: strapi.config.get("envs.nextjs.revalidateToken"),
+    //     },
+    //   }
+    // );
+    return;
   } catch (error) {
     console.error("Error revalidating Next.js ISR (on-demand)");
     console.error(error);
