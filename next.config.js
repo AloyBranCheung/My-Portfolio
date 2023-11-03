b78c15b20a9137dd221194eeddfb4dd818ded755
bcheung <brandon.cheung.dev@gmail.com>
2023-11-02 21:54:52 -0400

    add sentry auth token

--- next.config.js
+++ next.config.js
@@ -32,6 +32,7 @@ module.exports = withSentryConfig(
     silent: true,
     org: process.env.SENTRY_ORG,
     project: process.env.SENTRY_PROJECT,
+    authToken: process.env.SENTRY_AUTH_TOKEN,
   },
   {
     // For all available options, see:
